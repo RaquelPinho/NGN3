02bf4ac4c74ed201317694a40ded62a70e6d11c8
RaquelPinho <rdpinho@ucdavis.edu>
2021-02-11 15:39:02 -0800

    update chimeras

--- chimeras.R
+++ chimeras.R
@@ -6,6 +6,8 @@ library(CrispRVariants)
 library(GenomicAlignments)
 library(rBLAST)
 library(ggmsa)
+library(annotate)
+library(Biostrings)
 # loading the crisprset object
 load("D:/Raquel/Desktop/Post_doc/NGN3/NGN3_R/Data/CrisprSets_log_del.RData")
 # getting the crispr_set for NGN3
@@ -40,12 +42,43 @@ seq_mf_chimera <- sequences[most_frequent_chimera][1]
 
 # getting the DNA string of the most frequent chimera
 
-DNASt_mf_chimera <- 
+DNASt_mf_chimera <- dnas_chimeras_bl04[most_frequent_chimera[1]]
 
 # I will try to use the ggmsa to align the read of the most frequent quimera to the target and off target 
 
 # Fist I will load the .fasta files of the reference genome for the target / off-target region
+# saving the path
 ref_seq_long <- "./Data/ref_seq.fasta"
-
-
+# import the sequences as DNAstring set
+ref_seq_long <- readDNAStringSet(ref_seq_long, format = "fasta" )
+# transform strings into character vectors
+ref_seq_long_cha <- sapply(ref_seq_long, as.character)
 # we can then use blast to align the chimeras sequence to the reference sequence and figure it out if they are real duplicate or if we can align it to other parte of the genome
+
+### Using blast with the annotate package
+# http://rstudio-pubs-static.s3.amazonaws.com/12097_1352791b169f423f910d93222a4c2d85.html
+# this bioconductor package has a function `blastSequences` that sends a query for ncbi site an retrn the results.
+# I will try to use the sequence of most frequent quimera to test it:
+
+blast_mf <- blastSequences(seq_mf_chimera, database = "nr")
+head(blast_mf)
+blast_mf <- blastSequences(seq_mf_chimera, database = "nr", hitListSize = 2)
+blast_mf
+
+### Using the pairwiseAlignment function from BioStrings
+
+# To use this function we need to use the DNAString/ DNAStringSet objects
+Biostrings::pairwiseAlignment(DNASt_mf_chimera,ref_seq_long[[1]])
+
+# To do the pairwise alignemet with the sequences of the off-target sites
+
+AlignTest <- lapply(ref_seq_long, FUN = function(ref) {x = pairwiseAlignment(DNASt_mf_chimera, ref)
+
+# This is an object PairwiseAlignmentsSingleSubject and there are function to used in this type of the object
+
+as.matrix(AlignTest)
+nchar(AlignTest)
+
+
+
+
